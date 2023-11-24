fa58b4d7ae0a6e8d43e3b7688a55f61f69db671b
Karthik2701 <102400241+Karthik2701@users.noreply.github.com>
2023-11-23 19:26:26 -0800

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world thank you once again!'
+    return 'Hello, world thank you once again and wish you all the best!'
 
 if __name__ == '__main__':
     app.run()
